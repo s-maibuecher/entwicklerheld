8c13ca346ba9516ac4edd94a51c64c350a91bd19
Stefan Maibücher <s.maibuecher@gmail.com>
2020-02-18 17:31:30 +0100

    weiter dran gearbeitet

---
+++ waterpumps/stefans_test_file.py
@@ -0,0 +1,37 @@
+from waterpumps import exmaples
+from waterpumps.task import WaterPumpAnalyzer
+import arrow
+from datetime import datetime
+from dateutil import tz
+
+if __name__ == '__main__':
+    '''
+    # Level 1:
+    analyzer = WaterPumpAnalyzer()
+
+    analyzer.handle_message({'time': '2020-02-10T11:33:00.299356+01:00', 'device': 'pump', 'location': 'Hamburg',
+                             'energy_consumption': 1269})
+
+    test_set = exmaples.two_locations_no_error[:100]
+
+    for msg in test_set:
+        analyzer.handle_message(msg)
+
+    print(analyzer.pump_data_container)
+    print(analyzer.rain_gauge_data_container)
+
+    print(analyzer.get_raw_data(timestamp='2020-02-10T11:33:00.299356+01:00', device="pump", location="Hamburg"))
+    '''
+
+    # Level 2:
+    analyzer = WaterPumpAnalyzer()
+    for data in exmaples.two_locations_no_error:
+        analyzer.handle_message(data)
+
+    start = arrow.get(datetime(2020, 2, 13), tz.gettz('Europe/Berlin')).date()
+    end = arrow.get(datetime(2020, 2, 14), tz.gettz('Europe/Berlin')).date()
+
+    print(analyzer.is_error_mode(start, end, "Berlin"))
+
+    print(analyzer.is_error_mode(start, end, "Hamburg"))
+
--- waterpumps/task.py
+++ waterpumps/task.py
@@ -1,18 +1,19 @@
 import datetime
 import numpy as np
 import arrow
-
+from collections import defaultdict
 
 class WaterPumpAnalyzer:
 
     def __init__(self):
         # Create your storage attributes here.
 
-        self.data_container = {}
+        self.pump_data_container = defaultdict(dict)
+        self.rain_gauge_data_container = defaultdict(dict)
 
         # todo besser timedelta nehmen?
-        self.data_array = np.zeros((5,), dtype=[('time', 'datetime64[s]'), ('loc', 'U20'), ('is_pump', '?'), ('energy_consumption', 'f8'), ('value', 'i4')])
-        self.count_entries = 0
+        # self.data_array = np.zeros((10000,), dtype=[('time', 'datetime64[s]'), ('loc', 'U20'), ('is_pump', '?'), ('energy_consumption', 'f8'), ('value', 'i4')])
+        # self.count_entries = 0
         pass
 
     def handle_message(self, data: dict):
@@ -47,33 +48,118 @@ class WaterPumpAnalyzer:
             http://chrisschell.de/2018/02/01/how-to-efficiently-deal-with-huge-Numpy-arrays.html
 
         '''
+        # is_pump = True if data['device'] == 'pump' else False
+        #
+        # energy_consumption = data['energy_consumption'] if is_pump else 0.0
+        #
+        # value = 0 if is_pump else data['value']
+        #
+        # try:
+        #     self.data_array[self.count_entries] = (np.datetime64(data['time']), data['location'], is_pump, energy_consumption, value)
+        # except IndexError:
+        #     print('###'*10)
+        #     print(self.data_array.shape)
+        #     shape_ = self.data_array.shape[0]
+        #     self.data_array.resize((shape_ + 100000,), refcheck=False )
+        #     self.data_array[self.count_entries] = (
+        #         np.datetime64(data['time']), data['location'], is_pump, energy_consumption, value)
+        # self.count_entries +=1
+        #
+        # # --> https://stackoverflow.com/a/12285656/2952486
+        #
+        # ### HIER WEITER ######### --> ich glaube ein dict mit Städte Keys, value ist eine Datei, die als Einträge die einzelnen Tage hat
+
+        '''
+        Ich arbeite mit der Annahme, dass die Devices keine alten Stati mehr hochschicken:
+        dh. ich speicher die Daten fortführend in Tagesintervallen wie folgt ab:
+        
+        pump_data_container = {
+            'Köln': {
+                '20200101' : dateiverweis, # alle Meldungen dieses Tages schon erhalten -> wird aus dem RAM gesourced, um Platz freizumachen
+                '20200102' : dateiverweis, # alle Meldungen dieses Tages schon erhalten -> wird aus dem RAM gesourced, um Platz freizumachen
+                '20200103' : [(<uhrzeit>, <energie_consumption>), ...], # aktuellster Array wird noch laufend erweitert
+            },
+            'Düsseldorf': {
+                #...
+            },
+            # ...
+        }
+        
+        '''
+
         is_pump = True if data['device'] == 'pump' else False
+        stadt_name = data['location']
+        time_data = data['time']
 
-        energy_consumption = data['energy_consumption'] if is_pump else 0.0
+        day_string = time_data[:10].replace('-', '')
 
-        value = 0 if is_pump else data['value']
 
-        try:
-            self.data_array[self.count_entries] = (np.datetime64(data['time']), data['location'], is_pump, energy_consumption, value)
-        except IndexError:
-            print('###'*10)
-            print(self.data_array.shape)
-            shape_ = self.data_array.shape[0]
-            self.data_array.resize((shape_ + 10000000,), refcheck=False )
-            self.data_array[self.count_entries] = (
-                np.datetime64(data['time']), data['location'], is_pump, energy_consumption, value)
-        self.count_entries +=1
+        if is_pump:
+            data_container = self.pump_data_container
+            data_value = data['energy_consumption']
+        else: # -> rain_gauge
+            data_container = self.rain_gauge_data_container
+            data_value = data['value']
 
-        # --> https://stackoverflow.com/a/12285656/2952486
+        if not day_string in data_container[stadt_name]: # True, wenn der Tag noch nicht angelegt wurde
+            # dann leg den Tag an:
+            data_container[stadt_name][day_string] = [(time_data[10:], data_value)]
 
-        ### HIER WEITER ######### --> ich glaube ein dict mit Städte Keys, value ist eine Datei, die als Einträge die einzelnen Tage hat
+            # todo hier Vortag testen und falls abgeschlossen serialisieren
+        else:
+            # Tag existiert schon:
+            data_container[stadt_name][day_string].append((time_data[10:], data_value))
+
+    # https://stackoverflow.com/a/947184/2952486
+    # Stops iterating through the list as soon as it finds the value
+    def getIndexOfTuple(self, l, index, value):
+        for pos, t in enumerate(l):
+            if t[index] == value:
+                return pos
+
+        # Matches behavior of list.index
+        raise ValueError("list.index(x): x not in list")
+
+        # getIndexOfTuple(tuple_list, 0, "cherry")  # = 1
 
     def get_raw_data(self, timestamp: str, device: str, location: str) -> dict:
         # Implement this in Scenario 1
+        # Beispielaufruf: get_raw_data(timestamp='2020-02-10T11:33:00.299356+01:00', device="pump",
+        #                                        location="Hamburg")
 
-        return {}
+        is_pump = True if device == 'pump' else False
+        stadt_name = location
+        time_data = timestamp
+
+        day_string = time_data[:10].replace('-', '')
+        uhrzeit = time_data[10:]
+
+        if is_pump:
+            data_container = self.pump_data_container
+
+        else:  # -> rain_gauge
+            data_container = self.rain_gauge_data_container
+
+
+        # hier die Abfrage:
+        liste_mit_tageseintraegen = data_container[stadt_name][day_string]
+
+        # todo hier ist auf jeden Fall noch Optimierungspotenzial, Sortieralgorithmus...
+        result= liste_mit_tageseintraegen[self.getIndexOfTuple(liste_mit_tageseintraegen, 0, uhrzeit)][1]
+
+        return result
+        #return {}
+
+
 
     def is_error_mode(self, start: datetime.date, end: datetime.date, location: str) -> bool:
         # Implement this in Scenario 2,3 and 4
+        #print(start, end)
+
+        delta = end - start
+
+        for i in range(delta.days + 1):
+            day = start + datetime.timedelta(days=i)
+            print(day)
 
         pass
